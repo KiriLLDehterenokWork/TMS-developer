d100859c7c7586920a9c5078a69cf66dfb889500
Kirill <kdehterenok@gmail.com>
2026-05-03 14:52:38 +0300

    Added HW16

--- src/Task1/Main1HW16.java
+++ src/Task1/Main1HW16.java
@@ -2,6 +2,20 @@ package Task1;
 
 import static Task1.WordMultiple.wordMultiple;
 
+/**
+ * Дехтерёнок Кирилл
+ */
+
+/*
+Задача 1:
+На вход поступает массив строк, верните Map<String, Boolean>, где каждая отдельная
+строка является ключом, и ее значение равно true, если эта строка встречается в массиве
+2 или более раз. Пример:
+wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}
+wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}
+wordMultiple(["c", "c", "c", "c"]) → {"c": true}
+ */
+
 public class Main1HW16 {
     public static void main(String[] args){
         System.out.println(wordMultiple(new String[]{"a", "b", "a", "c", "b"}));
--- src/Task2/Main2HW16.java
+++ src/Task2/Main2HW16.java
@@ -2,6 +2,20 @@ package Task2;
 
 import static Task2.Pairs.pairs;
 
+/**
+ * Дехтерёнок Кирилл
+ */
+
+/*
+Задача 2:
+На вход поступает массив непустых строк, создайте и верните Map<String,
+String> следующим образом: для каждой строки добавьте ее первый символ в
+качестве ключа с последним символом в качестве значения. Пример:
+pairs(["code", "bug"]) → {"b": "g", "c": "e"}
+pairs(["man", "moon", "main"]) → {"m": "n"}
+pairs(["man", "moon", "good", "night"]) → {"g": "d", "m": "n", "n": "t"}
+ */
+
 public class Main2HW16 {
     public static void main(String[] args){
         System.out.println(pairs(new String[]{"code", "bug"}));
